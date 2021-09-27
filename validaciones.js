79c6ee2f1bf0d65375e0386c94b5c1c196fb9f8f
Felipe <fquiceno1@misena.edu.co>
2021-09-27 18:56:00 -0500

    Sprint 2 terminado

--- validaciones.js
+++ validaciones.js
@@ -1,7 +1,22 @@
-function validar_nombreUsuario(string){
-    document.write("user");
-}
+var expresiones = /^[a-zA-Z0-9\_\-]{4,9}$/;
+var expresiones1 = /^[a-zA-Z0-9\_\-]{5,30}$/;
 
-function validar_contrasena(string){
-    document.write("<br>password");
+function validar_nombreUsuario(usuario){
+    if(!expresiones.test(usuario) ||  usuario === ""){
+        return false;
+    }else{
+        return true;
+    }
 }
+function validar_contrasena(contra){
+    if(!expresiones1.test(contra) || contra === ""){
+        return false;
+    }else{
+        return true;
+    }
+}
+module.exports.validar_nombreUsuario = validar_nombreUsuario;
+module.exports.validar_contrasena = validar_contrasena;
+
+
+
