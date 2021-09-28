0fb570451fc143d16a53a62873f4d498f5eca5de
AndresMahechaM <elsencillo88@gmail.com>
2021-09-27 20:10:15 -0500

    Se crea el archivo registro.js con los nombres de las funciones y estructura solicitada en el Sprint 3.

---
+++ registro.js
@@ -0,0 +1,15 @@
+
+let registros = [];
+
+function agregarRegistro(){
+    //codigo
+}
+
+function filtrarPorContrasena(arreglo, filtro){
+    //codigo
+}
+
+
+module.exports.registros = registros;
+module.exports.filtrarPorContrasena = filtrarPorContrasena;
+module.exports.agregarRegistro = agregarRegistro;
