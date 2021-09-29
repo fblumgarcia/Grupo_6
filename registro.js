4a9d16283d4d9691326791944c1a5e4b2316535e
HeidyJimenez <90729440+HeidyJimenez@users.noreply.github.com>
2021-09-29 15:48:24 -0500

    Sprint 3

--- registro.js
+++ registro.js
@@ -1,12 +1,25 @@
-
 let registros = [];
+let registros2 = [];
 
 function agregarRegistro(){
     //codigo
+    registros.push(document.getElementById("in_usuario").value, document.getElementById("in_contrasena").value);
+    console.log(registros);
+    filtrarPorContrasena(registros, 5);
 }
 
-function filtrarPorContrasena(arreglo, filtro){
-    //codigo
+function filtrarPorContrasena(registros, filtro){
+     //codigo
+     registros2 = []
+     for (let i = 1, registro; registro = registros[i]; i++) {
+         if (registros[i].length <= filtro){
+            registros2.push(registros[i-1], registros[i])
+            i++;
+         }else{
+            i++;
+         }
+      }
+     console.log(registros2)
 }
 
 
