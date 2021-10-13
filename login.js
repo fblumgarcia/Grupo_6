81bc0b3b0297ead274e227164f1fc7b74ac3b8b6
Fernando Blum Garcia <fblumgarcia0920@gmail.com>
2021-10-12 21:55:34 -0500

    Se crea el documento login.js y se inicia el código

---
+++ login.js
@@ -0,0 +1,30 @@
+let registros = [];
+
+function validar_captcha(rcaptcha){
+    if(rcaptcha==5){//Valida que la respuesta captcha sea 5
+        login_captcha=true;
+    }else{//si no lo pone como false
+        login_captcha=false;
+    }
+}
+
+function iniciar_sesion(usuario,contrasena,rcaptcha){
+    validar_captcha();//Llama la función validar_captcha
+    if(login_captcha==true){//si cumple
+        if(registros.filter(registro=>registro.usuario.includes(usuario))==true){//Busca en el arreglo el usuario
+            if(registros.filter(registro=>registro.usuario.includes(contrasena)==true)){//Si esta busca la contraseña sea correcta
+                return true;
+            }else{//Si no es contraseña
+                return false;
+            }
+        }else{//Si no esta el usuario
+            return false;
+        }
+    }else{// Si el captcha esta mal
+        return false;
+    }
+}
+
+module.exports.registros = registros;
+module.exports.validar_captcha = validar_captcha;
+module.exports.iniciar_sesion = iniciar_sesion;
